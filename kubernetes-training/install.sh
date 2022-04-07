19b42042ce83a446c05e440a38327fbc37c1837a
Boubacar Sidy Diallo <bsdiallo07@gmail.com>
2022-04-07 23:48:36 +0200

    Argo-cd and contour

--- kubernetes-training/install.sh
+++ kubernetes-training/install.sh
@@ -1,1 +1,5 @@
+# NGINX
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.1.3/deploy/static/provider/cloud/deploy.yaml
+
+#CONTOUR
+ kubectl apply -f https://projectcontour.io/quickstart/contour.yaml
